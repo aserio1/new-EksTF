82a63a520874b1ed8428fee1b7901a3e0115306f
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:35:23 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -7,13 +7,13 @@ resource "aws_eks_cluster" "this" {
 
   encryption_config {
     provider {
-      key_arn = aws_kms_key.eks.arn
+      key_arn = aws_kms_key.iac_kms_key.arn
     }
     resources = ["secrets"]
   }
 
   vpc_config {
-    subnet_ids              = var.private_subnet_ids
+    subnet_ids              = var.private_subnets
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
     security_group_ids      = [aws_security_group.eks_cluster.id]
@@ -23,29 +23,46 @@ resource "aws_eks_cluster" "this" {
     aws_cloudwatch_log_group.eks
   ]
 
-  tags = local.common_tags
+  tags = merge(
+    {
+      Name        = local.cluster_name
+      Description = "EKS cluster"
+      Project     = var.project_name
+      Application = var.project_name
+    },
+    var.tags
+  )
 }
 
 resource "aws_eks_node_group" "primary" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${local.cluster_name}-primary"
+  node_group_name = "${var.project_name}-primary"
   node_role_arn   = var.node_role_arn
-  subnet_ids      = var.private_subnet_ids
+  subnet_ids      = var.private_subnets
+  instance_types  = var.node_instance_types
+  capacity_type   = "ON_DEMAND"
 
   scaling_config {
-    desired_size = var.node_desired_size
-    min_size     = var.node_min_size
-    max_size     = var.node_max_size
+    desired_size = var.desired_count
+    min_size     = var.min_capacity
+    max_size     = var.max_capacity
   }
-
-  instance_types = var.node_instance_types
-  capacity_type  = "ON_DEMAND"
 
   update_config {
     max_unavailable = 1
   }
 
-  tags = merge(local.common_tags, {
-    Name = "${local.cluster_name}-primary"
-  })
+  tags = merge(
+    {
+      Name        = "${var.project_name}-primary-ng"
+      Description = "Primary EKS managed node group"
+      Project     = var.project_name
+      Application = var.project_name
+    },
+    var.tags
+  )
+
+  depends_on = [
+    aws_eks_cluster.this
+  ]
 }
