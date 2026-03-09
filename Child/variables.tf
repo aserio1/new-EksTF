d83ddc7c6fa4b6d834ecb120e32385934e2f052a
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 17:03:48 -0500

    Update variables.tf

--- Child/variables.tf
+++ Child/variables.tf
@@ -1,57 +1,180 @@
-variable "region" {
-  type    = string
-  default = "us-gov-west-1"
+variable "aws_account_id" {
+  description = "The AWS Account ID for the deployment"
+  type        = string
 }
 
-variable "environment" {
-  type    = string
-  default = "primary"
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
 }
 
-variable "name_prefix" {
-  type    = string
-  default = "alfa"
+variable "alert_email" {
+  description = "Email address for SNS notification"
+  type        = string
+}
+
+variable "iam_role" {
+  description = "Primary deploy IAM role"
+  type        = string
+}
+
+variable "alb_log_reader_arns" {
+  description = "Role ARNs that can read ALB logs"
+  type        = list(string)
+  default     = []
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+}
+
+variable "branch" {
+  description = "Branch running the job"
+  type        = string
+  default     = "main"
 }
 
 variable "vpc_id" {
-  type = string
+  description = "The ID of the VPC"
+  type        = string
 }
 
-variable "private_subnet_ids" {
-  type = list(string)
+variable "public_subnets" {
+  description = "List of public subnet IDs for the ALB"
+  type        = list(string)
 }
 
-variable "public_subnet_ids" {
-  type    = list(string)
-  default = []
+variable "private_subnets" {
+  description = "List of private subnet IDs for EKS"
+  type        = list(string)
+}
+
+variable "container_name" {
+  description = "Application container name reference"
+  type        = string
+}
+
+variable "container_image" {
+  description = "Application container image reference"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Application container port"
+  type        = number
+}
+
+variable "cpu" {
+  description = "Application CPU reference value"
+  type        = number
+}
+
+variable "memory" {
+  description = "Application memory reference value"
+  type        = number
+}
+
+variable "desired_count" {
+  description = "Desired node count for EKS managed node group"
+  type        = number
+}
+
+variable "max_capacity" {
+  description = "Maximum node count for EKS managed node group"
+  type        = number
+}
+
+variable "min_capacity" {
+  description = "Minimum node count for EKS managed node group"
+  type        = number
+}
+
+variable "enable_efs" {
+  description = "Enable EFS resources"
+  type        = bool
+  default     = false
+}
+
+variable "efs_mount_point" {
+  description = "Reserved application mount point reference"
+  type        = string
+  default     = "/mnt/efs"
+}
+
+variable "efs_container_path" {
+  description = "Reserved application EFS path reference"
+  type        = string
+  default     = "efs-storage"
+}
+
+variable "certificate_arn" {
+  description = "ACM certificate ARN for HTTPS listener"
+  type        = string
+  default     = null
 }
 
 variable "cluster_version" {
-  type    = string
-  default = "1.29"
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.29"
 }
 
 variable "node_instance_types" {
-  type    = list(string)
-  default = ["m5.large"]
+  description = "EKS managed node group instance types"
+  type        = list(string)
+  default     = ["m5.large"]
 }
 
-variable "node_desired_size" {
-  type    = number
-  default = 2
+variable "container_environment_variables" {
+  description = "Optional application environment variables reference"
+  type        = any
+  default     = []
 }
 
-variable "node_min_size" {
-  type    = number
-  default = 2
+variable "container_secrets" {
+  description = "Optional application secrets reference"
+  type        = any
+  default     = []
 }
 
-variable "node_max_size" {
-  type    = number
-  default = 5
+variable "alb_ingress_rules" {
+  description = "List of ingress rules for the ALB"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "alb_egress_rules" {
+  description = "List of egress rules for the ALB"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "eks_node_egress_rules" {
+  description = "List of egress rules for the EKS node security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "alb_access_log_audit_bucket" {
+  description = "Bucket where ALB log bucket access logs are stored"
+  type        = string
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Common tags"
+  type        = map(string)
+  default     = {}
 }
