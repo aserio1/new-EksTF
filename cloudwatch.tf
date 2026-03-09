05c2f106acc4870584800945dce832f686084284
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:24:07 -0500

    Update cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,13 @@
 resource "aws_cloudwatch_log_group" "eks" {
   name              = "/aws/eks/${local.cluster_name}/cluster"
   retention_in_days = var.cloudwatch_log_retention_days
-  kms_key_id        = aws_kms_key.eks.arn
-  tags              = local.common_tags
+  kms_key_id        = aws_kms_key.iac_kms_key.arn
+
+  tags = merge(
+    {
+      Name        = "${var.project_name}-eks-logs"
+      Description = "EKS control plane logs"
+    },
+    var.tags
+  )
 }
