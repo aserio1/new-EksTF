f764c3b88153557cebabe0025094419e2bf778ba
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:34:50 -0500

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -1,42 +1,53 @@
 resource "aws_efs_file_system" "this" {
-  count          = var.create_efs ? 1 : 0
-  creation_token = "${local.cluster_name}-efs"
-  encrypted      = true
-  kms_key_id     = aws_kms_key.eks.arn
+  provider         = aws.eks-role
+  count            = var.enable_efs ? 1 : 0
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+  encrypted        = true
+  kms_key_id       = aws_kms_key.iac_kms_key.arn
 
-  tags = merge(local.common_tags, {
-    Name = "${local.cluster_name}-efs"
-  })
-}
-
-resource "aws_security_group" "efs" {
-  count       = var.create_efs ? 1 : 0
-  name        = "${local.cluster_name}-efs-sg"
-  description = "EFS security group"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description     = "NFS from EKS nodes"
-    from_port       = 2049
-    to_port         = 2049
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = local.common_tags
+  tags = merge(
+    {
+      Name        = "${var.project_name}-efs"
+      Description = "Application EFS"
+    },
+    var.tags
+  )
 }
 
 resource "aws_efs_mount_target" "this" {
-  for_each = var.create_efs ? toset(var.private_subnet_ids) : toset([])
-
+  provider        = aws.eks-role
+  count           = var.enable_efs ? length(var.private_subnets) : 0
   file_system_id  = aws_efs_file_system.this[0].id
-  subnet_id       = each.value
+  subnet_id       = var.private_subnets[count.index]
   security_groups = [aws_security_group.efs[0].id]
+}
+
+resource "aws_efs_access_point" "this" {
+  provider       = aws.eks-role
+  count          = var.enable_efs ? 1 : 0
+  file_system_id = aws_efs_file_system.this[0].id
+
+  posix_user {
+    gid = 1000
+    uid = 1000
+  }
+
+  root_directory {
+    path = "/export"
+
+    creation_info {
+      owner_gid   = 1000
+      owner_uid   = 1000
+      permissions = "755"
+    }
+  }
+
+  tags = merge(
+    {
+      Name        = "${var.project_name}-efs-access-point"
+      Description = "EFS access point"
+    },
+    var.tags
+  )
 }
