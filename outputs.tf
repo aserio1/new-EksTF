b3480808d3d32e3c76deb93774f1240d6e9b24f0
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:37:17 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,64 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
 }
 
 output "cluster_arn" {
-  value = aws_eks_cluster.this.arn
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.this.arn
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_certificate_authority_data" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
+  description = "EKS cluster certificate authority data"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
 }
 
-output "cluster_security_group_id" {
-  value = aws_security_group.eks_cluster.id
-}
-
-output "node_security_group_id" {
-  value = aws_security_group.eks_nodes.id
+output "node_group_name" {
+  description = "Primary node group name"
+  value       = aws_eks_node_group.primary.node_group_name
 }
 
 output "alb_arn" {
-  value = aws_lb.internal.arn
+  description = "ALB ARN"
+  value       = aws_lb.this.arn
 }
 
 output "alb_dns_name" {
-  value = aws_lb.internal.dns_name
+  description = "ALB DNS name"
+  value       = aws_lb.this.dns_name
 }
 
-output "alb_logs_bucket_name" {
-  value = aws_s3_bucket.alb_logs.bucket
+output "alb_target_group_arn" {
+  description = "ALB target group ARN"
+  value       = aws_lb_target_group.this.arn
 }
 
-output "kms_key_arn" {
-  value = aws_kms_key.eks.arn
+output "s3_bucket_name" {
+  description = "ALB access log bucket name"
+  value       = aws_s3_bucket.alb_access_log.bucket
 }
 
 output "efs_id" {
-  value = try(aws_efs_file_system.this[0].id, null)
+  description = "EFS file system ID"
+  value       = try(aws_efs_file_system.this[0].id, null)
+}
+
+output "efs_access_point_id" {
+  description = "EFS access point ID"
+  value       = try(aws_efs_access_point.this[0].id, null)
+}
+
+output "kms_key_arn" {
+  description = "KMS key ARN"
+  value       = aws_kms_key.iac_kms_key.arn
+}
+
+output "alb_sns_topic_arn" {
+  description = "SNS topic ARN for ALB log notifications"
+  value       = aws_sns_topic.alb_log_sns_topic.arn
 }
