1f2eab7161ea5ee3d5b33f97662b40bbcc0e49fb
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 13:22:23 -0500

    Create autoscale.tf

---
+++ autoscale.tf
@@ -0,0 +1,26 @@
+resource "aws_eks_addon" "coredns" {
+  cluster_name  = aws_eks_cluster.this.name
+  addon_name    = "coredns"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name  = aws_eks_cluster.this.name
+  addon_name    = "kube-proxy"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = aws_eks_cluster.this.name
+  addon_name    = "vpc-cni"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  tags = local.common_tags
+}
