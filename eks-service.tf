4d693d7c9a757f9444c764be0274012f4d69ee8a
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:36:29 -0500

    Update eks-service.tf

--- eks-service.tf
+++ eks-service.tf
@@ -2,10 +2,6 @@ data "aws_eks_cluster" "this" {
   name = aws_eks_cluster.this.name
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
-}
-
 resource "aws_eks_access_entry" "admin_role" {
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = var.admin_role_arn
@@ -15,6 +11,22 @@ resource "aws_eks_access_entry" "admin_role" {
 resource "aws_eks_access_policy_association" "admin_role_admin" {
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = var.admin_role_arn
+  policy_arn    = "arn:aws-us-gov:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_eks_access_entry" "deploy_role" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = var.iam_role
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "deploy_role_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = var.iam_role
   policy_arn    = "arn:aws-us-gov:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
