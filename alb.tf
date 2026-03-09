9e719edaab3497934be05c576217096545562d59
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:34:12 -0500

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,17 +1,105 @@
-resource "aws_lb" "internal" {
-  name               = substr(replace("${local.cluster_name}-alb", "/[^a-zA-Z0-9-]/", ""), 0, 32)
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.eks_nodes.id]
-  subnets            = var.private_subnet_ids
+resource "aws_lb" "this" {
+  name                       = "${var.project_name}-alb-sdo"
+  internal                   = true
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.alb.id]
+  subnets                    = var.public_subnets
+  drop_invalid_header_fields = true
+  enable_deletion_protection = false
 
   access_logs {
-    bucket  = aws_s3_bucket.alb_logs.id
-    prefix  = "alb"
+    bucket  = aws_s3_bucket.alb_access_log.bucket
     enabled = true
   }
 
-  tags = merge(local.common_tags, {
-    Name = "${local.cluster_name}-alb"
-  })
+  depends_on = [
+    aws_s3_bucket_policy.alb_access_log
+  ]
+
+  tags = merge(
+    {
+      Name        = "${var.project_name}-alb-sdo"
+      Description = var.project_name
+      Project     = var.project_name
+      Application = var.project_name
+    },
+    var.tags
+  )
+}
+
+resource "aws_lb_target_group" "this" {
+  name_prefix = substr("${var.project_name}-tg-", 0, 6)
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = merge(
+    {
+      Name        = "${var.project_name}-tg"
+      Description = "EKS application target group placeholder"
+    },
+    var.tags
+  )
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "http_forward" {
+  count             = var.certificate_arn == null ? 1 : 0
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  count             = var.certificate_arn != null ? 1 : 0
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+
+  depends_on = [
+    aws_lb_target_group.this
+  ]
 }
