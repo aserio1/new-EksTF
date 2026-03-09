53a2483b82c6ed097f321b9796d5e2cc4f22c558
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 17:05:35 -0500

    Update outputs.tf

--- Child/outputs.tf
+++ Child/outputs.tf
@@ -1,27 +1,37 @@
-output "cluster_name" {
-  value = module.eks_primary.cluster_name
+output "application_url" {
+  description = "URL to access the deployed application"
+  value       = var.certificate_arn != null ? "https://${module.eks_primary.alb_dns_name}" : "http://${module.eks_primary.alb_dns_name}"
 }
 
-output "cluster_arn" {
-  value = module.eks_primary.cluster_arn
+output "eks_cluster_identity" {
+  description = "EKS Cluster Name"
+  value       = module.eks_primary.cluster_name
+}
+
+output "storage_details" {
+  description = "S3 and EFS details"
+  value = {
+    s3_bucket = module.eks_primary.s3_bucket_name
+    efs_id    = module.eks_primary.efs_id
+  }
+}
+
+output "kms_key" {
+  description = "KMS key ARN"
+  value       = module.eks_primary.kms_key_arn
 }
 
 output "cluster_endpoint" {
-  value = module.eks_primary.cluster_endpoint
+  description = "EKS cluster endpoint"
+  value       = module.eks_primary.cluster_endpoint
+}
+
+output "node_group_name" {
+  description = "EKS managed node group name"
+  value       = module.eks_primary.node_group_name
 }
 
 output "alb_dns_name" {
-  value = module.eks_primary.alb_dns_name
-}
-
-output "alb_logs_bucket_name" {
-  value = module.eks_primary.alb_logs_bucket_name
-}
-
-output "kms_key_arn" {
-  value = module.eks_primary.kms_key_arn
-}
-
-output "efs_id" {
-  value = module.eks_primary.efs_id
+  description = "ALB DNS name"
+  value       = module.eks_primary.alb_dns_name
 }
